47752137f4e8533c8854bfd8dfeec6e4d2660f8f
Greg Khmara <gkhmara@gmail.com>
2022-03-26 18:13:09 -0700

    fix indentation

--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ import './css/styles.css';
 import ExchangeRate from './js/business.js';
 
 function clearFields() {
-  // $('#searchKey').val("");
   $('.showImages').text("");
   $('.showResults').text("");
 }
 
-
 function getElements(response, keyword, currency) {
   if (response.result === "success") {
     let output = keyword * response.conversion_rates[currency];
-    $('.showResults').text(output);
-      if (response.conversion_rates[currency] === undefined) {
-        $('.showResults').text('The selection you have made is not a valid currency. Please select another currency and try again.');
-      }
+    $('.showResults').text(output + " " + $("input:radio[name=currency]:checked").val());
+    if (response.conversion_rates[currency] === undefined) {
+      $('.showResults').text('The selection you have made is not a valid currency. Please select another currency and try again.');
+    }
   } else if (response.result === "error") {
     $('.showResults').text(`There was an error processing your request: ${response['error-type']}`);
   }
@@ -29,9 +27,9 @@ $(document).ready(function() {
     const currency = $("input:radio[name=currency]:checked").val();
     clearFields();
     ExchangeRate.getCurrency()
-    .then(function(response) {
-      getElements(response, keyword, currency);
-    });
+      .then(function(response) {
+        getElements(response, keyword, currency);
+      });
   });
 });
 
